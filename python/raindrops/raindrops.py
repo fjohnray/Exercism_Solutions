5de37e6f2a8d0a5a1c9ed5eedba1d4cbdfc90781
John Ray Fajutag <fjohnray@gmail.com>
2021-05-03 00:42:22 +0800

    python_exercise raindrops_v2

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,16 +1,10 @@
+sounds = {3: "Pling", 5: "Plang", 7: "Plong"}
+
 def convert(number):
 
     # returns raindrop "sounds" of either "Pling", "Plang", and/or "Plong"
     # if the number is divisible by 3, 5, and 7, respectively.
     # returns the number if the number is not divisible by either 3, 5, or 7
 
-    divisor = {3: "Pling", 5: "Plang", 7: "Plong"}
-    times = 0
-    raindrop_sound = ""
-
-    for digit in divisor.keys():
-        if number % digit == 0:
-            raindrop_sound = raindrop_sound + divisor[digit]
-            times += 1
-
-    return f"{raindrop_sound}" if times != 0 else f"{number}"
+    drop_sound = [value for key, value in sounds.items() if number % key == 0]
+    return "".join(drop_sound) if drop_sound else str(number)
