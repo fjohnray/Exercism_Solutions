7debe590607f2eb2b08273cdc9f37915e88e961a
John Ray Fajutag <fjohnray@gmail.com>
2021-05-03 01:17:05 +0800

    python_exercise raindrops_v3

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,10 +1,16 @@
 sounds = {3: "Pling", 5: "Plang", 7: "Plong"}
 
+
 def convert(number):
 
     # returns raindrop "sounds" of either "Pling", "Plang", and/or "Plong"
-    # if the number is divisible by 3, 5, and 7, respectively.
-    # returns the number if the number is not divisible by either 3, 5, or 7
+    # if the {argument} is a number divisible by 3, 5, and 7, respectively.
+    # returns the {argument} if it is a number but is not divisible by either 3, 5, or 7
+    # returns "The passed argument was not a number" if the {argument} is a string type
 
-    drop_sound = [value for key, value in sounds.items() if number % key == 0]
-    return "".join(drop_sound) if drop_sound else str(number)
+    if type(number) != str:
+        drop_sound = [value for key, value in sounds.items() if number % key == 0]
+        wife = "".join(drop_sound) if drop_sound else str(number)
+        return wife
+    else:
+        return "The passed argument was not a number."
